b727d3010283fc8d6ceb8b4e6febe6124c6f2288
ElGibran17 <ibrahimelgibran17@gmail.com>
2023-12-20 12:31:16 +0700

    update

--- src/data/data.ts
+++ src/data/data.ts
@@ -42,4 +42,103 @@ export const myProjects = [
     git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
     techUsed: ["Next JS", "Tailwind CSS", "Framer Motion"],
   },
+  {
+    title: "Youtube Clone No Ads - WebApp",
+    description:
+      "This is a IegcodeTube is a youtube's clone website that i've created using React JS, Material UI and RapidAPI as data API for learning purposes.",
+    image: "project-3.png",
+    link: "https://iegcodetube.vercel.app/",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["React JS", "Material UI", "RapidAPI"],
+  },
+  {
+    title: "Profil and PPDB Online - WebApp",
+    description:
+      "This is a Website Profile PPDB 2024-2025 project that I created using Codeigniter 3, Bootstrap, MySQL.",
+    image: "project-2.png",
+    link: null,
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["Codeigniter", "Bootstrap", "MySQL"],
+  },
+  {
+    title: "CMS School - WebApp",
+    description:
+      "This is a CMS ESKAMUGA project that I created using PHP, Codeignither 4 and MySQL.",
+    image: "project-4.png",
+    link: null,
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["Codeigniter", "Bootstrap", "MySQL"],
+  },
+  {
+    title: "Management Cource Data - WebApp",
+    description:
+      "This is a Management of Cource Student Data With System Dashbaord, CRUD and Tech Stack PHP Versi 8, MYSQL, CSS, BOOTSTRAP 5.",
+    image: "project-5.png",
+    link: null,
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["PHPv8", "Bootstrap", "MySQL"],
+  },
+  {
+    title: "Portfolio Personal v2 - WebApp",
+    description:
+      "This is a personal branding project that I created using Tailwind CSS and Bootstap.",
+    image: "project-6.png",
+    link: "https://ibrahimelgibran.github.io/PersonalTemplate/",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["Tailwind CSS", "Bootstrap"],
+  },
+  {
+    title: "GitHub Clone - WebApp",
+    description:
+      "This is a Implement an API fetch of your GitHub portfolio, show a page with a list of all your repositories on GitHub, and show another page linking to each repo using nested routes while using all the necessary tools in react.",
+    image: "project-12.png",
+    link: "https://iegcodegithub.vercel.app/",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["ReactJS", "Vanila CSS"],
+  },
+  {
+    title: "Profil Restaurant - WebApp",
+    description:
+      "This is a personal branding project that I created using Tailwind CSS and Bootstap.",
+    image: "project-7.png",
+    link: "https://sotoijomonjali.com/",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+  },
+  {
+    title: "CBT Online - WebApp",
+    description:
+      "This is a CBT online SMK MUHAMMADIYAH GAMPING for students ESKAMUGA with own fiture complite, i created using tech stack Codeigniter 4, CSS, Bootstrap, JavaScript, MYSQL.",
+    image: "project-9.png",
+    link: null,
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["Codeigniter 4", "Bootstrap", "MYSQL"],
+  },
+  {
+    title: "Invite Wedding - WebApp",
+    description:
+      "This is a Web Wedding digital and many fiture complite like mp3, maps, photo, O'clock, chat live congratulations.",
+    image: "project-8.png",
+    link: "https://iegcodewedding.vercel.app/",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+  },
+  {
+    title: "Link In Bio - WebApp",
+    description:
+      "This is a Create Link in Bio page from my profile Instagram tech stack HTML, CSS and JavaScript.",
+    image: "project-10.png",
+    link: "https://iegcodelink.vercel.app/",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+  },
+  {
+    title: "Invite Wedding v2 - WebApp",
+    description:
+      "TThis is a Web Wedding version 2 and own fiture mp3, O'clock, Maps, Save date, gallery photo, animation loading dll.",
+    image: "project-11.png",
+    link: "https://iegcodeweddingv2.vercel.app/",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Assalamualaiku%20%0A%0ASekian%20Terima%20Kasih%20Wassalamualaikum%20Wr.Wb",
+    techUsed: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+  },
 ];
