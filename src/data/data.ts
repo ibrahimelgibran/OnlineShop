8a0bc9b4916fcc76e271c0062872ef76e73cbb41
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-04-09 17:23:07 +0700

    update

--- src/data/data.ts
+++ src/data/data.ts
@@ -149,7 +149,7 @@ export const myProjects = [
       "This is a Free Access Course Online project that I created using Codeigniter, Bootstrap and MYSQL.",
     image: "project-017.png",
     link: null,
-    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%F0%9F%91%8B%F0%9F%8F%BB%2C%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20Learning%20Free%20Course%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga%2C%20fitur%20yang%20termasuk%2C%20dan%20proses%20kerjanya%3F%0A%0ATerima%20kasih!",
+    git: "https://api.whatsapp.com/send?phone=6285876076005&text=Hi%20IegCode%F0%9F%91%8B%F0%9F%8F%BB%2C%20Salam%20Kenal!%0A%0ASaya%20Tertarik%20dengan%20Paket%20E-Learning%20Free%20Course%20-%20WebApp%0A%0ABisakah%20Anda%20memberikan%20informasi%20lebih%20lanjut%20mengenai%20harga%2C%20fitur%20yang%20termasuk%2C%20dan%20proses%20kerjanya%3F%0A%0ATerima%20kasih!",
     techUsed: ["REACTJS", "Vite"],
   },
   {
